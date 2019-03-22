a72aca8b2fe1de888dcfbc76574c14e8b082f2e5
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2019-03-22 01:56:20 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const ytdl = require("ytdl-core");
-const Util = require("Util");
+const Util = require("discord.js");
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 const YouTube = require('simple-youtube-api');
