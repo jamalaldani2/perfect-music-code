a6bd8d7ccc50981b1d63ae87cd6dfc415b987094
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2019-03-22 01:50:38 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ const prefix = `$`
  
  
  
-client.on('message', async message => {
+client.on('message', async msg => {
   if (msg.author.bot) return undefined;
   if (!msg.content.startsWith(prefix)) return undefined;
   const args = msg.content.split(' ');
